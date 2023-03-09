c98ca2762a888e7eb7d6a558c1a85a255f07b173
Marco D'Agostini <madacol10@gmail.com>
2023-03-09 15:21:27 +0000

    Can now edit security pass after right clicking

--- security pass/index.js
+++ security pass/index.js
@@ -53,37 +53,38 @@ security_pass_template = `<html lang="en">
     <table>
         <tr>
             <td>Guest:</td>
-            <td>{guest}</td>
+            <td contenteditable>{guest}</td>
         </tr>
         <tr>
             <td>Reference:</td>
-            <td>{reference}</td>
+            <td contenteditable>{reference}</td>
         </tr>
         <tr>
             <td>Room:</td>
-            <td>{room}</td>
+            <td contenteditable>{room}</td>
         </tr>
         <tr>
             <td>Bed:</td>
-            <td>{bed}</td>
+            <td contenteditable>{bed}</td>
         </tr>
         <tr>
             <td>Arrival:</td>
-            <td>{arrival}</td>
+            <td contenteditable>{arrival}</td>
         </tr>
         <tr>
             <td>Departure:</td>
-            <td>{departure}</td>
+            <td contenteditable>{departure}</td>
         </tr>
     </table>
     <hr>
 
-    <h5>Free Breakfast from: 7.30 AM - 10 AM</h5>
-    <h5><strong>Check-out time: 10 AM</strong></h5>
-    <h5>WI-FI password: bananaman</h5>
-    <h5>Cooking time: 11 AM - 10 PM</h5>
-    <h5>Access CODE for lockers: C234</h5>
-    <h5><strong>😊 PUB CRAWL TICKETS ON SALE 😊</strong></h5>
+    <h5 contenteditable>Free Walking tours at 11 AM and 1 PM</h5>
+    <h5 contenteditable>Free Breakfast from: 7.30 AM - 10 AM</h5>
+    <h5 contenteditable><strong>Check-out time: 10 AM</strong></h5>
+    <h5 contenteditable>WI-FI password: bananaman</h5>
+    <h5 contenteditable>Cooking time: 11 AM - 10 PM</h5>
+    <h5 contenteditable>Access CODE for lockers: C234</h5>
+    <h5 contenteditable><strong>😊 PUB CRAWL TICKETS ON SALE 😊</strong></h5>
     <br>
     <br>
     .
@@ -92,14 +93,59 @@ security_pass_template = `<html lang="en">
 
 reservations = Array.from(document.querySelectorAll("[data-test-reservation-card]"));
 
-iframe = document.getElementById("security_pass_print");
-if (iframe === null) {
-    iframe = document.createElement("iframe");
-    iframe.id = "security_pass_print";
-    iframe.style.position = "fixed";
-    iframe.style.margin = "50px";
-    // iframe.style.visibility = "hidden";
-    document.body.appendChild(iframe);
+/** @type {HTMLElement} */
+/*let container;*/
+
+container = document.getElementById("security_pass_print");
+if (container === null) {
+    container = document.createElement("div");
+    container.id = "security_pass_print";
+    container.style.zIndex = 10001;
+    container.style.position = "fixed";
+    container.style.height = "100vh";
+    container.style.width = "100vw";
+    container.style.display = "flex";
+    container.style.justifyContent = "center";
+    container.style.alignItems = "stretch";
+    container.style.visibility = "hidden";
+    document.body.appendChild(container);
+    
+    const backdrop = document.createElement("div");
+    backdrop.style.backgroundColor = "gray";
+    backdrop.style.opacity = 0.5;
+    backdrop.style.flexGrow = 1;
+    backdrop.onclick = () => container.style.visibility = "hidden";
+    container.appendChild(backdrop);
+
+    const iframe_container = document.createElement("div");
+    iframe_container.style.display = "flex";
+    iframe_container.style.flexBasis = "515px";
+    iframe_container.style.flexDirection = "column";
+
+    {    
+        const iframe = document.createElement("iframe");
+        iframe.style.flexGrow = "1";
+        iframe_container.appendChild(iframe);
+
+        const button = document.createElement("button");
+        button.innerText = "Print Security Pass";
+        button.style.fontSize = "2em";
+        button.style.padding = "0.5em";
+        button.onclick = () => iframe.contentWindow.print();
+        iframe_container.appendChild(button);
+    };
+    container.appendChild(iframe_container);
+
+    const backdrop_clone = backdrop.cloneNode();
+    backdrop_clone.onclick = () => container.style.visibility = "hidden";
+    container.appendChild(backdrop_clone);
+
+
+    document.addEventListener("keydown", event => {
+        console.log(event.key)
+        if (event.key !== "Escape") return;
+        container.style.visibility = "hidden";
+    }, true);
 };
 
 reservations.forEach(reservation => {
@@ -110,15 +156,15 @@ reservations.forEach(reservation => {
                                     .outerText.split(" - ");
     const guest = nameReversed.split(', ').reverse().join(" ");
     const reference = Array.from(reservation.querySelectorAll(".short-hand > span"))
-                            .find(el => el.outerText.match(/[0-9]{5,6}/))
+                            .find(el => el.outerText.match(/[0-9]{5}/))
                             .outerText;
     /** @type string */
     let [room, bed] = roombed.split("-");
     if (room.toLowerCase() === "kr") room = "Kinlay Room";
-    if (bed === undefined) bed = "All";
+    if (bed === undefined) bed = "Private Room";
     const [arrival, departure] = reservation.querySelector("[class^=Caption] > span")
                                             .outerText.split(" ‐ ");
- 
+
     /**
      * embedding data into template
      */
@@ -132,12 +178,27 @@ reservations.forEach(reservation => {
 
     /**
      * Make *print button* print the security pass
+     * @type {HTMLElement}
      */
     const button = reservation.querySelector('[data-test-icon-button="print"]');
     button.addEventListener("click", event => {
+        const iframe = container.querySelector("iframe");
+        iframe.contentDocument.open();
+        iframe.contentDocument.write(security_pass);
+        iframe.contentWindow.print();
+    });
+    button.addEventListener("contextmenu", event => {
+        event.preventDefault();
+        container.style.visibility = "visible";
+        const iframe = container.querySelector("iframe");
         iframe.contentDocument.open();
         iframe.contentDocument.write(security_pass);
         iframe.focus();
-        iframe.contentWindow.print();
+
+        iframe.contentDocument.addEventListener("keydown", event => {
+            console.log(event.key)
+            if (event.key !== "Escape") return;
+            container.style.visibility = "hidden";
+        }, true);
     });
 });

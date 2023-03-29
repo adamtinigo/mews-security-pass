90635e517a6416ffc070ae8f29e6707a279afa72
Marco D'Agostini <madacol10@gmail.com>
2023-03-29 14:00:23 +0100

    auto mapping is much faster
    It can now wait for elements to be rendered

--- auto key mappings.js
+++ auto key mappings.js
@@ -1,40 +1,60 @@
-const iframe = document.createElement("iframe")
-document.body.append(iframe)
-
-function wait(conditionCallback, delay = 1000) {
-    return new Promise((resolve, reject) => {
-        let counter = 0
-        function resolveIfCondition() {
-            setTimeout(() => {
-                if (conditionCallback()) return resolve()
-
-                if (++counter > 30) return console.error(`Max counter reached (${counter}): condition was never met\nconditionCallback: "${conditionCallback}" `);
-                resolveIfCondition()
-            }, delay);
-        }
-        resolveIfCondition()
-    })
-}
-
-(async ()=>{
-
-    const beds_to_change = Array.from(document.querySelectorAll('tr > td:first-child a'))
-    .filter(x=>x.outerText.match(/[\dK][\dR]\d?-\d\d/));
-
-    for (const bed of beds_to_change) {
-        iframe.contentWindow.location = bed.href
-        console.log('passing...')
-        await wait(()=>(iframe.contentDocument.querySelectorAll('input#Name') !== null))
-        console.log('pass')
-        const input = iframe.contentDocument.querySelector('input#Name')
-        const newValue = input.value.replace('-','.')
-        
-        input.focus()
-        input.click()
-        input.value = newValue
-        input.dispatchEvent(new InputEvent("beforeinput", {inputType: "insertText", data: newValue, bubbles: true, cancelable: true}))
-        input.dispatchEvent(new InputEvent("input", {inputType: "insertText", data: newValue, bubbles: true}))
-        input.dispatchEvent(new InputEvent("change", {bubbles: true}))
-        iframe.contentDocument.querySelector('button[type="submit"]').click()
+{
+    function getElementNotYetRendered(elementGetter, delay = 100, timeout = 10000) {
+        let retries = Math.ceil(timeout / delay);
+        return new Promise((resolve, reject) => {
+            (function resolveIfElementFound() {
+                setTimeout(() => {
+                    const element = elementGetter()
+                    if (element?.toString().includes("Element")) return resolve(element)
+                    if (element?.toString().includes("NodeList") && element.length > 0) return resolve(element)
+                    
+                    if (retries-- <= 0) return console.error(`Max retries reached: element was not found
+                    element: "${element}"
+                    elementGetter: "${elementGetter}"
+                    `);
+                    resolveIfElementFound()
+                }, delay);
+            })()
+        })
     }
-})()
+    
+    function most_foreground_modal() { return document.querySelector('[class^="style__ModalTransitionGroup"] > [id^="mgr-0-modal"]:last-child') }
+    
+    async function getFirstOptionToChange() {
+        /**
+         * This select is too optimized and only shows options currently in view,
+         * so we have to get and process its options in chunks
+         */
+    
+        while (true) {
+            const options_chunk = await getElementNotYetRendered(()=>document.querySelectorAll('[role="listbox"] [role="option"]'))
+            console.log(options_chunk);
+            const options_chunk_array = Array.from(options_chunk)
+            const option = options_chunk_array.find(option => !option.outerText.includes("."));
+            if (option) return option
+            
+            /* last_option */ options_chunk_array.at(-1).scrollIntoView()
+        }
+    }
+    
+    (async ()=>{
+    
+        while (true) {
+            /* click add_button */ (await getElementNotYetRendered(()=>most_foreground_modal()?.querySelector('[data-test-icon-button="add_box"]'))).click()
+        
+            // open select input
+            const room_select = await getElementNotYetRendered(()=>most_foreground_modal()?.querySelector('[data-test-select-name="Resource.Id"]'))
+            /* click room_select */ room_select.click()
+        
+            const option = await getFirstOptionToChange()
+            console.log(option.outerText);
+            if (!option) return
+        
+            option.click()
+            const input = document.getElementById("LockIdentifier")
+            input.value = `${option.outerText}.01`
+            const send_button = most_foreground_modal()?.querySelector('button[type="submit"]').click()
+        }
+    
+    })()
+}
